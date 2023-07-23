eeadb3cbcd89fef2a1464304a01618d6a4f3459b
Vasilis Georgitzikis <tzikis@gmail.com>
2023-07-23 17:23:48 -0500

    Updated vscode script to use new parameters.yml.dist

--- scripts/vscode_docker_scripts.sh
+++ scripts/vscode_docker_scripts.sh
@@ -9,7 +9,7 @@ echo 127.0.0.1   dev.codebender.cc >> /etc/hosts
 echo 127.0.0.1   compiler.dev.codebender.cc >> /etc/hosts
 
 cd /opt/codebender/$PACKAGENAME
-cp symfony-configs/compiler-parameters.yml Symfony/config/parameters.yml
+cp Symfony/config/parameters.yml.dist Symfony/config/parameters.yml
 
 cd Symfony
 
